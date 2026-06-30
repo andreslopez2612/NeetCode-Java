d9ba9a3b633fc399eb80f88a89dcb82229fdb321
Andres Lopez <andreslo2612@gmail.com>
2026-06-30 09:38:52 -0500

    Add productExceptSelf.java implementation for array product calculation

---
+++ src/main/java/productExceptSelf.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+public class productExceptSelf {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 4, 6};
+        //Output: [48,24,12,8]
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+
+}
